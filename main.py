f54ffe167e10ffdef8f2785694a255cbe00c5c69
KILLSCARY <zabivnoy.grisha@yandex.ru>
2025-08-09 00:56:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8091412559:AAHgqI_YrIiVrgIQ5jWhmMvtaV_2aSglNrg"  # Заменить на свой
+TOKEN = "8091412559:AAHgqI_YrIiVrgIQ5jWhmMvtaV_2aSglNrg"  # вставь свой токен
 
-# Команда /start
+# Обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я бот.")
+    await update.message.reply_text("Привет! Это мини-игра с кракеном 🐙")
 
 # Основной запуск
 def main():
@@ -15,5 +15,5 @@ def main():
 
     app.run_polling()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
